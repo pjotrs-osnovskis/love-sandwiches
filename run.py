34e257621e958f6e9d5d8b3b2188c0419e094b3c
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-08-27 21:57:41 +0100

    Set up gspread and spreadsheet connection

--- run.py
+++ run.py
@@ -1,1 +1,30 @@
-print("Hello World")
+"""
+First I used pip3 install gspread google-auth command in VSCode terminal to install gspread library
+Then importing gspread
+"""
+import gspread
+
+# Then Used this line to access Credentials class
+from google.oauth2.service_account import Credentials
+
+# Here we use constant SCOPE (constants written in CAPITAL LETTERS) to access certain google services
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+# Passing creds.json file for credentials to work
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+
+# GSPREAD client
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+
+# Accessing Love Sandwiches spreadsheet
+SHEET = GSPREAD_CLIENT.open("love_sandwiches")
+
+# Testing connection to spreadsheet
+sales = SHEET.worksheet("sales")
+data = sales.get_all_values()
+print(data)
