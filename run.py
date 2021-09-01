560842ebc7fe0f1d25d37c5909a8c9fc2f271acb
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-09-01 19:55:13 +0100

    Adjust validate_data function to check if all input data is ints.

--- run.py
+++ run.py
@@ -57,10 +57,11 @@ def validate_data(values):
     # print(values)
 
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"6 values required, you provided: {len(values)}"
-            )1,
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please check data and try again.\n")
 
